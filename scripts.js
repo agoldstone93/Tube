7431d8bcb88a6843d957e67b1d89ffb7edaa6547
Adam <13471320+agoldstone93@users.noreply.github.com>
2021-04-17 09:37:46 +0100

    getting the refresh button working

--- scripts.js
+++ scripts.js
@@ -5,34 +5,34 @@ app.controller('myController', function($scope, $http, $interval){
         console.log("refresh");
     }
 
+    
     var errorCallback = function (response){
         $scope.lines = response.statusText;
     }
-
+    
     $scope.getData = function() {
         $http({
             method: 'GET',
             //url: 'http://127.0.0.1:6500/index.html' //test bad gateway
-             url: 'https://api.tfl.gov.uk/Line/Mode/tube/Status'
+            url: 'https://api.tfl.gov.uk/Line/Mode/tube/Status'
         }).then(successCallback, errorCallback);
         $scope.currentTime = new Date().toLocaleTimeString();
         $scope.currentDate = new Date().toLocaleDateString();
     }
-    
     $scope.getData();
     
+    var promise;
     $scope.intervalFunction = function() {
-        $interval(function() {
+        promise = $interval(function() {
             $scope.getData();            
-        }, 30000);
+        }, 300000);
     }
-
     $scope.intervalFunction();
     
-    // not working yet
-    $scope.refresh = function() {
-        $interval.cancel
+    $scope.refreshPage = function() {
         console.log("BUTTON");
+        $interval.cancel(promise);
+        $scope.getData();
         $scope.intervalFunction();
     }
 });
